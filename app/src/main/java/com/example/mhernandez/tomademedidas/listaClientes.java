fff1626bc818ff71917ff00c20400e0b48cabbf1
mhernandez <23>
2017-10-04 13:31:57 -0600

    layout listaproyectos

--- app/src/main/java/com/example/mhernandez/tomademedidas/listaClientes.java
+++ app/src/main/java/com/example/mhernandez/tomademedidas/listaClientes.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -12,21 +14,34 @@ import android.widget.EditText;
 
 public class listaClientes extends AppCompatActivity {
 
+    public static DBProvider oDB;
+
+    public listaClientes() { oDB = new DBProvider(this);}
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_cliente);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle oExt = this.getIntent().getExtras();
+        final String idCliente = oExt.getString("idCliente");
+        final String idDisp = oExt.getString("idDisp");
         String nom = oExt.getString("nombre");
         String tel = oExt.getString("telefono");
         String dir = oExt.getString("direccion");
-        EditText nombre = (EditText) this.findViewById(R.id.txtNombre);
-        EditText telefono = (EditText) this.findViewById(R.id.txtTelefono);
-        EditText direccion = (EditText) this.findViewById(R.id.txtDireccion);
+        final EditText nombre = (EditText) this.findViewById(R.id.txtNombre);
+        final EditText telefono = (EditText) this.findViewById(R.id.txtTelefono);
+        final EditText direccion = (EditText) this.findViewById(R.id.txtDireccion);
+        Button Guardar = (Button) this.findViewById(R.id.Guardar);
         nombre.setText(nom);
         telefono.setText(tel);
         direccion.setText(dir);
+        Guardar.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        oDB.updateCliente(idCliente,idDisp,nombre.getText().toString(),telefono.getText().toString(),direccion.getText().toString());
+                    }
+                });
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
