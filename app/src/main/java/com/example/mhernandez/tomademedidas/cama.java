4ebbcae51353e1219fd799ea7a689baa37b3e4f4
mhernandez <23>
2017-10-16 16:56:57 -0600

    insertProyecto Cama, Especial y Galeria

--- app/src/main/java/com/example/mhernandez/tomademedidas/cama.java
+++ app/src/main/java/com/example/mhernandez/tomademedidas/cama.java
@@ -3,6 +3,9 @@ package com.example.mhernandez.tomademedidas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by mhernandez on 16/10/2017.
@@ -18,13 +21,42 @@ public class cama extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crear_cama);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Bundle oExt = this.getIntent().getExtras();
+        final Bundle oExt = this.getIntent().getExtras();
         final String idFormato = oExt.getString("idFormato");
         final String nombreProyecto = oExt.getString("nombreProyecto");
         final String accesoriosMuro = oExt.getString("accesoriosMuro");
         final String accesoriosTecho = oExt.getString("accesoriosTecho");
         final String accesoriosEspecial = oExt.getString("accesoriosEspecial");
-        final String PedidoSap = oExt.getString("PedidoSap");    }
+        final String PedidoSap = oExt.getString("PedidoSap");
+        final EditText NHabitaciones = (EditText) this.findViewById(R.id.txt_numero_habitaciones);
+        final EditText A = (EditText) this.findViewById(R.id.txt_A);
+        final EditText B = (EditText) this.findViewById(R.id.txt_B);
+        final EditText C = (EditText) this.findViewById(R.id.txt_C);
+        final EditText D = (EditText) this.findViewById(R.id.txt_D);
+        final EditText E = (EditText) this.findViewById(R.id.txt_E);
+        final EditText F = (EditText) this.findViewById(R.id.txt_F);
+        final EditText G = (EditText) this.findViewById(R.id.txt_G);
+        final EditText Observaciones = (EditText) this.findViewById(R.id.txt_observaciones);
+        Button Guardar = (Button) this.findViewById(R.id.Guardar);
+        Guardar.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        String numeroHabitaciones = NHabitaciones.getText().toString();
+                        String txtA = A.getText().toString();
+                        String txtB = B.getText().toString();
+                        String txtC = C.getText().toString();
+                        String txtD = D.getText().toString();
+                        String txtE = E.getText().toString();
+                        String txtF = F.getText().toString();
+                        String txtG = G.getText().toString();
+                        String OBS = Observaciones.getText().toString();
+/*                        oDB.insertProyecto();
+                        oDB.insertProyectoCama();*/
+                        finish();
+                    }
+                }
+        );
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
