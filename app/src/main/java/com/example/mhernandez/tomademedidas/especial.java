08ef79db7b195019d23a4e8a7d4be134248cac85
mhernandez <23>
2017-12-05 09:46:03 -0700

    Imagen en creacion de medidas por camara o por imagen existente en galeria del dispositivo

--- app/src/main/java/com/example/mhernandez/tomademedidas/especial.java
+++ app/src/main/java/com/example/mhernandez/tomademedidas/especial.java
@@ -2,20 +2,37 @@ package com.example.mhernandez.tomademedidas;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 /**
  * Created by mhernandez on 16/10/2017.
  */
 
 public class especial extends AppCompatActivity {
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final String APP_PATH = "TomaMedidas";
+    private Uri fileUri;
+    int auxFoto=0;
+    String nombreImagen="1";
+    String imagen = "";
 
     public static DBProvider oDB;
     public especial() {oDB = new DBProvider(this);}
@@ -45,6 +62,14 @@ public class especial extends AppCompatActivity {
         final EditText Grosor = (EditText) this.findViewById(R.id.txtGrosor);
         final EditText Observaciones = (EditText) this.findViewById(R.id.txtObservaciones);
         Button Guardar = (Button) this.findViewById(R.id.Guardar);
+        String[][] aRefD = MainActivity.oDB.lastDispositivo();
+        String[][] aRefP = MainActivity.oDB.lastProyecto();
+
+        final int idProyecto = Integer.parseInt(aRefP[(0)][0]) + 1;
+        final int idDisp = Integer.parseInt(aRefD[(0)][0]);
+        nombreImagen = "" + idProyecto + idDisp;
+
+        Button Imagenes = (Button) this.findViewById(R.id.TomarFoto);
         Guardar.setOnClickListener(
                 new View.OnClickListener(){
                     public void onClick(View view){
@@ -52,21 +77,67 @@ public class especial extends AppCompatActivity {
                         Double txtAlto = Double.parseDouble(Alto.getText().toString());
                         Double txtGrosor = Double.parseDouble(Grosor.getText().toString());
                         String OBS = Observaciones.getText().toString();
-                        String[][] aRefD = MainActivity.oDB.lastDispositivo();
-                        String[][] aRefP = MainActivity.oDB.lastProyecto();
                         String[][] aRefE = MainActivity.oDB.lastEspecial();
-                        int idProyecto = Integer.parseInt(aRefP[(0)][0]) + 1;
                         int idEspecial = Integer.parseInt(aRefE[(0)][0]) + 1;
-                        int idDisp = Integer.parseInt(aRefD[(0)][0]);
                         oDB.insertProyecto(idProyecto, idDisp, idCliente, idclienteDisp, idFormato, usuario, nombreProyecto, PedidoSap, FechaAlta,
                                            0, accesoriosTecho, accesoriosMuro, accesoriosEspecial, 1, idUsuarioVenta, Agente, 1);
                         oDB.insertProyectoEspecial(idEspecial, idDisp, idProyecto, idDisp, nombreProyecto, txtAlto, txtAncho, txtGrosor,
-                        OBS, "IMAGEN", FechaAlta, idFormato, FechaAlta, 1, 0, 0, 0);
+                        OBS, imagen, FechaAlta, idFormato, FechaAlta, 1, 0, 0, 0);
                         Intent rIntent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(rIntent);
                     }
                 }
         );
+
+        ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);
+        final TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+
+        if(savedInstanceState != null){
+            fileUri = savedInstanceState.getParcelable("uri");
+            if(fileUri != null){
+                Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(savedInstanceState.getString("foto"),200,200);
+                oImg.setImageBitmap(bit_map);
+                foto.setText(imagen);
+            }
+        }
+
+
+        Imagenes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog = new Dialog(especial.this, R.style.Theme_Dialog_Translucent);
+                customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                customDialog.setContentView(R.layout.menu_tabla_imagenes);
+
+                ((Button) customDialog.findViewById(R.id.btnCamara)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent rIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE, nombreImagen);
+                        rIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(rIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                        customDialog.dismiss();
+                    }
+                });
+
+                ((Button) customDialog.findViewById(R.id.btnGaleria)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        auxFoto=2;
+                        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        startActivityForResult(gallery, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                        customDialog.dismiss();
+                        /*
+                        Intent rIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE, "");
+                        rIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(rIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);   */
+                    }
+                });
+                customDialog.show();
+            }
+        });
+
     }
 
     @Override
@@ -92,4 +163,119 @@ public class especial extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        if (fileUri != null) {
+            savedInstanceState.putParcelable("uri", fileUri);
+            savedInstanceState.getString("foto", fileUri.getPath());
+        }
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    /*
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }*/
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private static Uri getOutputMediaFileUri(int type, String pID){
+        return Uri.fromFile(getOutputMediaFile(type,pID));
+    }
+
+    private static File getOutputMediaFile(int type, String pID){
+        File mediaStorageDir = new
+                File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),APP_PATH);
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_ESPECIAL" + pID + ".jpg");
+        }else {
+            return null;
+        }
+        return mediaFile;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            if (auxFoto==2) {
+                if(resultCode == RESULT_OK && requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE){
+                    fileUri = data.getData();
+                    ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);
+                    oImg.setImageURI(fileUri);
+                    //Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(fileUri.getPath(),400,400);
+                    TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+                    oImg.buildDrawingCache();
+                    Bitmap bit_map = oImg.getDrawingCache();
+                    imagen = convertToBase64(bit_map);
+                    foto.setText(imagen);
+                }
+            }
+            else {
+                if (resultCode == RESULT_OK){
+                    ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);//
+                    Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(fileUri.getPath(),400,400);
+                    imagen = convertToBase64(bit_map);
+                    TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+                    foto.setText(imagen);
+                    oImg.setImageBitmap(bit_map);//
+                }else if(resultCode == RESULT_CANCELED){
+                    // User cancelled the image capture
+                }else {
+                    //Image capture failed, advise user
+                }
+            }
+        }
+    }
+
+
+    private String convertToBase64(Bitmap imagenMap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        imagenMap.compress(Bitmap.CompressFormat.JPEG, 45, baos);
+        byte[] byteArrayImage = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        return encodedImage;
+    }
+
 }
--- app/src/main/java/com/example/mhernandez/tomademedidas/medidaCama.java
+++ app/src/main/java/com/example/mhernandez/tomademedidas/medidaCama.java
@@ -1,15 +1,25 @@
 package com.example.mhernandez.tomademedidas;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -19,6 +29,13 @@ import java.util.Date;
 
 
 public class medidaCama extends AppCompatActivity {
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final String APP_PATH = "TomaMedidas";
+    private Uri fileUri;
+    int auxFoto=0;
+    String nombreImagen="1";
+    String imagen = "";
 
     public static DBProvider oDB;
     public medidaCama() {oDB = new DBProvider(this);}
@@ -42,6 +59,12 @@ public class medidaCama extends AppCompatActivity {
         final EditText F = (EditText) this.findViewById(R.id.txt_F);
         final EditText G = (EditText) this.findViewById(R.id.txt_G);
         final EditText Observaciones = (EditText) this.findViewById(R.id.txt_observaciones);
+
+        String[][] aRefD = MainActivity.oDB.lastDispositivo();
+        final int idDisp = Integer.parseInt(aRefD[(0)][0]);
+        nombreImagen=""+idProyecto+idDisp;
+
+        Button Imagenes = (Button) this.findViewById(R.id.TomarFoto);
         Button Guardar = (Button) this.findViewById(R.id.Guardar);
         Guardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,15 +80,66 @@ public class medidaCama extends AppCompatActivity {
                 Double txtF = Double.parseDouble(F.getText().toString());
                 Double txtG = Double.parseDouble(G.getText().toString());
                 String OBS = Observaciones.getText().toString();
-                String[][] aRefD = MainActivity.oDB.lastDispositivo();
                 String[][] aRefC = MainActivity.oDB.lastCama();
-                int idDisp = Integer.parseInt(aRefD[(0)][0]);
                 int idCama = Integer.parseInt(aRefC[(0)][0]) + 1;
                 oDB.insertProyectoCama(idCama, idDisp, idProyecto, idProyectoDisp, numeroHabitacion, txtA, txtB, txtC,
-                        txtD, txtE, txtF, txtG, FechaAlta, Nombre, 4, OBS, 0, 0, 1, 0, "", 1);
+                        txtD, txtE, txtF, txtG, FechaAlta, Nombre, 4, OBS, 0, 0, 1, 0, imagen, 1);
                 finish();
             }
         });
+
+
+        ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);
+        final TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+
+        if(savedInstanceState != null){
+            fileUri = savedInstanceState.getParcelable("uri");
+            if(fileUri != null){
+                Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(savedInstanceState.getString("foto"),200,200);
+                oImg.setImageBitmap(bit_map);
+                foto.setText(imagen);
+            }
+        }
+
+
+        Imagenes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog = new Dialog(medidaCama.this, R.style.Theme_Dialog_Translucent);
+                customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                customDialog.setContentView(R.layout.menu_tabla_imagenes);
+
+                ((Button) customDialog.findViewById(R.id.btnCamara)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent rIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE, nombreImagen);
+                        rIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(rIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                        customDialog.dismiss();
+                    }
+                });
+
+                ((Button) customDialog.findViewById(R.id.btnGaleria)).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        auxFoto=2;
+                        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                        startActivityForResult(gallery, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                        customDialog.dismiss();
+                        /*
+                        Intent rIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE, "");
+                        rIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(rIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);   */
+                    }
+                });
+                customDialog.show();
+            }
+        });
+
+
+
     }
 
     @Override
@@ -91,4 +165,119 @@ public class medidaCama extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        if (fileUri != null) {
+            savedInstanceState.putParcelable("uri", fileUri);
+            savedInstanceState.getString("foto", fileUri.getPath());
+        }
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    /*
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }*/
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    private static Uri getOutputMediaFileUri(int type, String pID){
+        return Uri.fromFile(getOutputMediaFile(type,pID));
+    }
+
+    private static File getOutputMediaFile(int type, String pID){
+        File mediaStorageDir = new
+                File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),APP_PATH);
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_CAMA" + pID + ".jpg");
+        }else {
+            return null;
+        }
+        return mediaFile;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            if (auxFoto==2) {
+                if(resultCode == RESULT_OK && requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE){
+                    fileUri = data.getData();
+                    ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);
+                    oImg.setImageURI(fileUri);
+                    //Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(fileUri.getPath(),400,400);
+                    TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+                    oImg.buildDrawingCache();
+                    Bitmap bit_map = oImg.getDrawingCache();
+                    imagen = convertToBase64(bit_map);
+                    foto.setText(imagen);
+                }
+            }
+            else {
+                if (resultCode == RESULT_OK){
+                    ImageView oImg = (ImageView) this.findViewById(R.id.imgFoto);//
+                    Bitmap bit_map = PictureTools.decodeSampledBitmapFromUri(fileUri.getPath(),400,400);
+                    imagen = convertToBase64(bit_map);
+                    TextView foto = (TextView) this.findViewById(R.id.TV_Imagen);
+                    foto.setText(imagen);
+                    oImg.setImageBitmap(bit_map);//
+                }else if(resultCode == RESULT_CANCELED){
+                    // User cancelled the image capture
+                }else {
+                    //Image capture failed, advise user
+                }
+            }
+        }
+    }
+
+
+    private String convertToBase64(Bitmap imagenMap) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        imagenMap.compress(Bitmap.CompressFormat.JPEG, 45, baos);
+        byte[] byteArrayImage = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        return encodedImage;
+    }
+
 }
